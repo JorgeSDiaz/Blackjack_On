d8d7eac389c46ed2ed1cf6661a19ded78da5ae3d
julian castillo <jul.1012@hotmail.com>
2023-05-06 18:23:43 -0500

    files_ready

---
+++ GameService_Blackjack_On/src/main/resources/static/client.js
@@ -0,0 +1,30 @@
+client = (() => {
+    // Private
+    const resource = "http://localhost:8080/v1/game";
+    const registerBet = (id, owner, token) => {
+        return new Promise( (resolve, reject) => {
+            $.ajax(
+                {
+                    url: resource + "/betBox/" + id + "/owner/" + owner,
+                    type: "POST",
+                    data: JSON.stringify(token),
+                    contentType: "application/json",
+                    success: (data) => {
+                        resolve(data);
+                    },
+                    error: (err) => {
+                        reject(err);
+                    }
+                }
+            )
+            }
+        );
+    };
+
+    // Public
+    return {
+        registerBet: (id, owner, token) => {
+            return registerBet(id, owner, token);
+        }
+    };
+})();
